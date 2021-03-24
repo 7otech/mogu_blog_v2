507c89c3ed125f5dd3cfcbb5de6971b1ed124a60
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-03-24 20:54:03 +0800

    update

--- uniapp_mogu_web/config/config.js
+++ uniapp_mogu_web/config/config.js
@@ -7,8 +7,8 @@ export const appConfig = {
 	PICTURE_API: 'http://localhost:8607/mogu-picture',
 	
 	// 演示环境
-	// WEB_API: 'http://120.78.126.96:8607/mogu-web',
-	// PICTURE_API: 'http://120.78.126.96:8607/mogu-picture',
+	// WEB_API: 'http://82.157.54.206:8607/mogu-web',
+	// PICTURE_API: 'http://82.157.54.206:8607/mogu-picture',
 	
 	// 正式环境
 	// WEB_API: 'https://apiweb.7otech.com/mogu-web',
